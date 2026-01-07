6ccc2983f8feff4c6d83aff3891ce85a143f606c
Vladimir <snermory@gmail.com>
2026-01-07 06:28:20 +0300

    Setup of the sandbox

---
+++ Greeting/greeting.cpp
@@ -0,0 +1,6 @@
+#include <iostream>
+#include "greeting.h"
+
+void Greet() {
+	std::cout << "Hello there! My name is Vladimir. What's yours?" << "\n";
+}
---
+++ main.cpp
@@ -0,0 +1,12 @@
+#include <cstdlib>
+#include <iostream>
+#include "greeting.h"
+
+int main() {
+	// std::cout << "Hello world!" << std::endl;
+	
+	Greet();
+
+	system("pause");
+	return 0;
+}
